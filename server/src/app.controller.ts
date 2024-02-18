a1f9c966c2d0689376ac2f2bfc0bcbe6974bc266
burshilya2024 <bursh2024@gmail.com>
2024-02-18 14:38:38 +0300

    installing a database via docker compose

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,12 +1,22 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-
+import { ApiOkResponse, ApiProperty } from '@nestjs/swagger';
+class HelloWorldDto {
+  // указываем swagger какой тип нужно сохранить
+  @ApiProperty()
+  message: string;
+}
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @ApiOkResponse({
+    type: HelloWorldDto,
+  })
+  async getHello(): Promise<HelloWorldDto> {
+    // const users = await this.dbService.user.findMany({});
+    // console.log(users);
+    return { message: this.appService.getHello() };
   }
 }
